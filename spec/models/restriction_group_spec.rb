06525039c6736e4b1cf9e245227192514e8a8878
Nicolas Iniesta <inicolas69@gmail.com>
2025-06-05 12:05:31 +0200

    Update RestrictionGroup relations

--- spec/models/restriction_group_spec.rb
+++ spec/models/restriction_group_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe RestrictionGroup, type: :model do
   describe "associations" do
     it { is_expected.to belong_to(:restrictable) }
+    it { is_expected.to have_many(:restrictions).dependent(:destroy) }
+    it { is_expected.to have_many(:restriction_groups).dependent(:destroy) }
   end
 
   describe "validations" do
